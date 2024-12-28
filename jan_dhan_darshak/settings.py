a909babaa7d09aaa51c39cb6f88e6ff4368de9ef
aryan shukla <aryanshukla212004@gmail.com>
2024-12-28 17:39:32 +0530

    Initial commit for backend code

---
+++ jan_dhan_darshak/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jan_dhan_darshak',
+        'USER': 'postgres',
+        'PASSWORD': 'aryan123',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
